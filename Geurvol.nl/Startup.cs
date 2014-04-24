dcfd8412760db73cfbe8cfbdbcaba36c0cc4af93
Jordi Röer <jordiroer@gmail.com>
2014-04-24 09:28:40 +0200

    Starting project

---
+++ Geurvol.nl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Geurvol.nl.Startup))]
+namespace Geurvol.nl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
